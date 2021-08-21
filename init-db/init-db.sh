5a20bbceeba301d13beabccb65f4b4cd40fcb100
Ulysse Goarant <ulysse.goarant@protonmail.com>
2021-08-21 21:19:30 +0200

    init-db: parametrize mongodb host

--- init-db/init-db.sh
+++ init-db/init-db.sh
@@ -4,8 +4,9 @@ set -e
 set -u
 
 readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+readonly MONGODB_HOST="localhost"
 readonly DB="whoisPresident"
 readonly COLLECTION="potentialCandidates"
 
-mongo "${DB}" ${SCRIPT_DIR}/create-index.js
-mongoimport --jsonArray --db "${DB}" --collection "${COLLECTION}" --file ${SCRIPT_DIR}/data/potential-candidates.json
+mongo "${MONGODB_HOST}/${DB}" ${SCRIPT_DIR}/create-index.js
+mongoimport --host "${MONGODB_HOST}" --jsonArray --db "${DB}" --collection "${COLLECTION}" --file ${SCRIPT_DIR}/data/potential-candidates.json
